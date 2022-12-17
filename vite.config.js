ed9955d94e8da0147b3610fe79c2a55cbf58c347
Wiktor Orda <wiktor.orda@gmail.com>
2022-12-17 21:53:51 +0100

    gh-pages update...

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { ghPages } from "vite-plugin-gh-pages";
 
 /** @type {import('vite').UserConfig} */
 export default {
-  base: "/Megamania-Atari-Game-In-The-Web/",
+  base: "./Megamania-Atari-Game-In-The-Web/",
   plugins: [ghPages()],
 };
